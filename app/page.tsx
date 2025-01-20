4354e61bdce32f96a4b7fc4838f9d1e339eaf52c
ari.utomo <itdev4.indorent@gmail.com>
2025-01-20 15:24:38 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
 'use client';
 
-import Header from "./components/header";
-import { useEffect } from "react";
-
 export default function Home() {
-
-  useEffect(() => {
-    const btnTest = document.getElementById("test");
-    if (btnTest) {
-      btnTest.addEventListener("click", () => {
-        alert("Test");
-      });
-    }
-  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen">
